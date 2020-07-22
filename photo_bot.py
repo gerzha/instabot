246354df0d39f1af6445db8b5d9bee15c7ecd153
dimas <d_dimas>
2020-07-22 22:12:38 +0300

    fix comments

--- photo_bot.py
+++ photo_bot.py
@@ -1,8 +1,8 @@
 from instapy import InstaPy
 from instapy import smart_run
 
-insta_username = "test"
-insta_password = "test"
+insta_username = "xxx"
+insta_password = "xxx"
 
 
 # get a session!
@@ -10,8 +10,8 @@ session = InstaPy(username=insta_username, password=insta_password, headless_bro
 
 # let's go! :>
 with smart_run(session):
-    other_locale = ['1023936095/', "790750200/minsk/", 'locations/1023936095/',  "229302938/gorky-park-minsk/"]
-    locations = []
+    other_locale = ['1023936095/', "790750200/minsk/", 'locations/1023936095/',  "229302938/gorky-park-minsk/", "500112008/"]
+    locations = ['790750200/minsk/']
 
     # general settings
     session.set_skip_users(skip_business=True, skip_no_profile_pic=True, skip_private=True)
@@ -51,7 +51,7 @@ with smart_run(session):
     #                      media='Photo')
     session.set_comments([
                              u'Это фото огонь! :heart_eyes: Если есть минутка '
-                             u'оцени мои',
+                             u'оцени мои :wink:',
                              u'Очень крутое фото, определенно лайка :heart_eyes:'
                              u'если есть минутка - оцени мои) :wink:',
                              u'Очень крутое фото :heart_eyes:  определенно лайк! ',
@@ -63,8 +63,31 @@ with smart_run(session):
                              u'Мне нравятся Ваши фото, а Вам мои?) :wink:',
                              u'Делайте больше таких постов, это круто! :wink:',
                              u'Ради этого стоило заходить на Вашу страничку. :smiley:'
-                             u'Как Вам моя?) :wink:'],
-                         media='Photo')
+                             u'Как Вам моя?) :wink:',
+                             u'Какая живая фотография!',
+                             u'Классный пост, так держать!',
+                             u'Какое фото :heart_eyes: :heart_eyes: :heart_eyes:',
+                             u'Красиво :smiley:',
+                             u'Прикольно:wink:',
+                             u'Однозначно круто!:smiley:',
+                             u'Фото классное, фотографу респект!',
+                             u'Хорошая фотография получилась на мой взгляд :smiley:',
+                             u'Фотография получилась очень даже огонь!:heart_eyes:',
+                             u'Прикольное, яркое у вас фото получилось!:ok_hand:',
+                             u'Фото класс :raised_hands:! Буду рада, если посмотрите мои',
+                             u'Вау, какое фото у вас! Мне нравятся такие. Возможно, Вам мои тоже понравятся :smiley:',
+                             u'Очень мило :smiley:',
+                             u'Красота в деталях :heart_eyes:',
+                             u'Красота в мелочах, здорово у Вас получилось :smiley:',
+                             u'За такую фотографию однозначно лайк :heart_eyes:',
+                             u'Смотрится очень классно!',
+                             u'Хороший кадр :+1:, мне нравятся такие. А как Вам мои?',
+                             u'Чудесные фото у вас!:smiley:',
+                             u'Хорошие фото у вас в профиле :smiley:',
+                             u'Побольше бы таких теплых фото, как у вас в профиле :smiley:'], media='Photo')
+
+
+
     session.set_do_like(True, percentage=70)
     session.set_delimit_liking(enabled=True, max_likes=200, min_likes=0)
     session.set_delimit_commenting(enabled=True, max_comments=20, min_comments=0)
@@ -92,5 +115,3 @@ with smart_run(session):
     """ Joining Engagement Pods...
     """
     session.join_pods(topic='general', engagement_mode='no_comments')
-
-    session.already_liked()
